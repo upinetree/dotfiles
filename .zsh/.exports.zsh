47edab6da9e3f5def822c6863fcb1cbc6e414eae
upinetree <t.matsumoto@everyleaf.com>
2016-05-17 02:15:12 +0000

    Tweek exports

--- .zsh/.exports.zsh
+++ .zsh/.exports.zsh
@@ -1,4 +1,4 @@
-export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 export PATH=$HOME/bin:$PATH
 export PATH=$HOME/.nodebrew/current/bin:$PATH
 
@@ -24,6 +24,7 @@ export LANG='ja_JP.UTF-8'
 export LC_ALL='ja_JP.UTF-8'
 export PAGER='less -Ou8'
 export EDITOR='vim'
+export LESSCHARSET=utf-8
 
 # ls colors
 export CLICOLOR=1
