b86fb2aeb01a9c3d8da8a74991e04ab96eb84832
upinetree <t.matsumoto@everyleaf.com>
2016-06-07 20:39:49 +0900

    Detect platform

--- etc/scripts/install.sh
+++ etc/scripts/install.sh
@@ -2,6 +2,8 @@ mkdir ~/.zsh
 mkdir ~/.vim
 mkdir ~/bin
 
+detect_platform
+
 ln -s ~/.dotfiles/.gemrc                      ~/.gemrc
 ln -s ~/.dotfiles/.gitconfig                  ~/.gitconfig
 ln -s ~/.dotfiles/.gitignore                  ~/.gitignore
@@ -9,9 +11,22 @@ ln -s ~/.dotfiles/.tmux.conf                  ~/.tmux.conf
 ln -s ~/.dotfiles/.zshrc                      ~/.zshrc
 ln -s ~/.dotfiles/.zsh/.aliases.zsh           ~/.zsh/.aliases.zsh
 ln -s ~/.dotfiles/.zsh/.exports.zsh           ~/.zsh/.exports.zsh
-ln -s ~/.dotfiles/osx/.slate                  ~/.slate
-ln -s ~/.dotfiles/osx/.vimperatorrc           ~/.vimperatorrc
-ln -s ~/.dotfiles/osx/bin/git-completion.bash ~/bin/git-completion.bash
-ln -s ~/.dotfiles/osx/bin/login-shell         ~/bin/login-shell
 ln -s ~/.dotfiles/.vimrc                      ~/.vimrc
 ln -s ~/.dotfiles/.vim/colors                 ~/.vim/colors
+
+if [ "$PLATFORM" = "osx" ]; then
+  ln -s ~/.dotfiles/osx/.slate                  ~/.slate
+  ln -s ~/.dotfiles/osx/.vimperatorrc           ~/.vimperatorrc
+  ln -s ~/.dotfiles/osx/bin/git-completion.bash ~/bin/git-completion.bash
+  ln -s ~/.dotfiles/osx/bin/login-shell         ~/bin/login-shell
+fi
+
+detect_platform() {
+  export PLATFORM
+  case "$(uname)" in
+    *Darwin*) PLATFORM='osx'     ;;
+    *Linux*)  PLATFORM='linux'   ;;
+    *)        echo 'Your platform is not supported'
+              exit 0 ;;
+  esac
+}
