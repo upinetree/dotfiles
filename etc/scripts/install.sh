3cac8fc9e687d9ddc5530cf0242dbd01f2a4d27b
upinetree <t.matsumoto@everyleaf.com>
2016-06-07 20:48:24 +0900

    Unlink existing dotfiles

--- etc/scripts/install.sh
+++ etc/scripts/install.sh
@@ -1,25 +1,7 @@
-mkdir ~/.zsh
-mkdir ~/.vim
-mkdir ~/bin
-
 detect_platform
-
-ln -s ~/.dotfiles/.gemrc                      ~/.gemrc
-ln -s ~/.dotfiles/.gitconfig                  ~/.gitconfig
-ln -s ~/.dotfiles/.gitignore                  ~/.gitignore
-ln -s ~/.dotfiles/.tmux.conf                  ~/.tmux.conf
-ln -s ~/.dotfiles/.zshrc                      ~/.zshrc
-ln -s ~/.dotfiles/.zsh/.aliases.zsh           ~/.zsh/.aliases.zsh
-ln -s ~/.dotfiles/.zsh/.exports.zsh           ~/.zsh/.exports.zsh
-ln -s ~/.dotfiles/.vimrc                      ~/.vimrc
-ln -s ~/.dotfiles/.vim/colors                 ~/.vim/colors
-
-if [ "$PLATFORM" = "osx" ]; then
-  ln -s ~/.dotfiles/osx/.slate                  ~/.slate
-  ln -s ~/.dotfiles/osx/.vimperatorrc           ~/.vimperatorrc
-  ln -s ~/.dotfiles/osx/bin/git-completion.bash ~/bin/git-completion.bash
-  ln -s ~/.dotfiles/osx/bin/login-shell         ~/bin/login-shell
-fi
+setup_dirs
+unlink_all
+link_all
 
 detect_platform() {
   export PLATFORM
@@ -29,4 +11,51 @@ detect_platform() {
     *)        echo 'Your platform is not supported'
               exit 0 ;;
   esac
+}
+
+setup_dirs() {
+  mkdir ~/.zsh
+  mkdir ~/.vim
+
+  if [ "$PLATFORM" = "osx" ]; then
+    mkdir ~/bin
+  fi
+}
+
+unllink_all() {
+  unlink ~/.gemrc
+  unlink ~/.gitconfig
+  unlink ~/.gitignore
+  unlink ~/.tmux.conf
+  unlink ~/.zshrc
+  unlink ~/.zsh/.aliases.zsh
+  unlink ~/.zsh/.exports.zsh
+  unlink ~/.vimrc
+  unlink ~/.vim/colors
+  unlink ~/.slate
+  unlink ~/.vimperatorrc
+
+  if [ "$PLATFORM" = "osx" ]; then
+    unlink ~/bin/git-completion.bash
+    unlink ~/bin/login-shell
+  fi
+}
+
+link_all() {
+  ln -s ~/.dotfiles/.gemrc                      ~/.gemrc
+  ln -s ~/.dotfiles/.gitconfig                  ~/.gitconfig
+  ln -s ~/.dotfiles/.gitignore                  ~/.gitignore
+  ln -s ~/.dotfiles/.tmux.conf                  ~/.tmux.conf
+  ln -s ~/.dotfiles/.zshrc                      ~/.zshrc
+  ln -s ~/.dotfiles/.zsh/.aliases.zsh           ~/.zsh/.aliases.zsh
+  ln -s ~/.dotfiles/.zsh/.exports.zsh           ~/.zsh/.exports.zsh
+  ln -s ~/.dotfiles/.vimrc                      ~/.vimrc
+  ln -s ~/.dotfiles/.vim/colors                 ~/.vim/colors
+
+  if [ "$PLATFORM" = "osx" ]; then
+    ln -s ~/.dotfiles/osx/.slate                  ~/.slate
+    ln -s ~/.dotfiles/osx/.vimperatorrc           ~/.vimperatorrc
+    ln -s ~/.dotfiles/osx/bin/git-completion.bash ~/bin/git-completion.bash
+    ln -s ~/.dotfiles/osx/bin/login-shell         ~/bin/login-shell
+  fi
 }
