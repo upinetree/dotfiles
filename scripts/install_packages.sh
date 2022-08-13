7048db516a90ba3550b42286baa69e06e177c798
upinetree <upinetree@gmail.com>
2022-08-13 18:32:49 +0900

    install karabiner-elements

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -127,7 +127,7 @@ if [ "$PLATFORM" = "osx" ]; then
     brew tap homebrew/cask-versions
 
     brew install anyenv bat coreutils ctags direnv git-delta gnu-sed git gh openssl readline ripgrep source-highlight tig tmux tree vim watch zsh
-    brew install alt-tab chromedriver dropbox iterm2 keepassx kap meetingbar slate
+    brew install alt-tab chromedriver dropbox iterm2 kap keepassx karabiner-elements meetingbar slate
   fi
 fi
 
