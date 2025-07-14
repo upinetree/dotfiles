332969d189209aef26baae8800f6d4482c2d6f5c
upinetree <upinetree@gmail.com>
2025-07-14 21:27:36 +0900

    dotfiles root is always specified

--- scripts/link.sh
+++ scripts/link.sh
@@ -3,6 +3,7 @@ set -u
 . ./scripts/lib.sh
 
 declare -A DOTFILE_MAPS
+declare DOTFILE_DIR="~/.dotfiles/"
 
 init() {
   detect_platform
@@ -38,39 +39,40 @@ unlink_all() {
 link_all() {
   for src in "${!DOTFILE_MAPS[@]}"; do
     dst="${DOTFILE_MAPS[$src]}"
-    log "ln -s $src $dst"
-    eval "ln -s $src $dst"
+    log "ln -s $DOTFILE_DIR$src $dst"
+    eval "ln -s $DOTFILE_DIR$src $dst"
   done
 }
 
 listup_dotfiles() {
+  # src (repo root) => dst
   DOTFILE_MAPS=(
-    ["~/.dotfiles/.gemrc"]="~/.gemrc"
-    ["~/.dotfiles/.gitconfig"]="~/.gitconfig"
-    ["~/.dotfiles/.gitignore"]="~/.gitignore"
-    ["~/.dotfiles/.ripgreprc"]="~/.ripgreprc"
-    ["~/.dotfiles/.tigrc"]="~/.tigrc"
-    ["~/.dotfiles/.tmux.conf"]="~/.tmux.conf"
-    ["~/.dotfiles/.vim/colors"]="~/.vim/colors"
-    ["~/.dotfiles/.vimrc"]="~/.vimrc"
-    ["~/.dotfiles/.zsh/.aliases.zsh"]="~/.zsh/.aliases.zsh"
-    ["~/.dotfiles/.zsh/.exports.zsh"]="~/.zsh/.exports.zsh"
-    ["~/.dotfiles/.zsh/.zplug.zsh"]="~/.zsh/.zplug.zsh"
-    ["~/.dotfiles/.zshrc"]="~/.zshrc"
-    ["~/.dotfiles/.config/nvim/init.vim"]="~/.config/nvim/init.vim"
-    ["~/.dotfiles/.config/alacritty/alacritty.toml"]="~/.config/alacritty/alacritty.toml"
-    ["~/.dotfiles/.config/mise/.config.toml"]="~/.config/mise/config.toml"
-    ["~/.dotfiles/default-gems"]="~/.config/mise/default-gems"
-    ["~/.dotfiles/.claude/settings.json"]="~/.claude/settings.json"
-    ["~/.dotfiles/.claude/commands"]="~/.claude/commands"
+    [".gemrc"]="~/.gemrc"
+    [".gitconfig"]="~/.gitconfig"
+    [".gitignore"]="~/.gitignore"
+    [".ripgreprc"]="~/.ripgreprc"
+    [".tigrc"]="~/.tigrc"
+    [".tmux.conf"]="~/.tmux.conf"
+    [".vim/colors"]="~/.vim/colors"
+    [".vimrc"]="~/.vimrc"
+    [".zsh/.aliases.zsh"]="~/.zsh/.aliases.zsh"
+    [".zsh/.exports.zsh"]="~/.zsh/.exports.zsh"
+    [".zsh/.zplug.zsh"]="~/.zsh/.zplug.zsh"
+    [".zshrc"]="~/.zshrc"
+    [".config/nvim/init.vim"]="~/.config/nvim/init.vim"
+    [".config/alacritty/alacritty.toml"]="~/.config/alacritty/alacritty.toml"
+    [".config/mise/.config.toml"]="~/.config/mise/config.toml"
+    ["default-gems"]="~/.config/mise/default-gems"
+    [".claude/settings.json"]="~/.claude/settings.json"
+    [".claude/commands"]="~/.claude/commands"
   )
 
   if [ "$PLATFORM" = "osx" ]; then
-    DOTFILE_MAPS["~/.dotfiles/osx/.slate"]="~/.slate"
-    DOTFILE_MAPS["~/.dotfiles/osx/.vimperatorrc"]="~/.vimperatorrc"
-    DOTFILE_MAPS["~/.dotfiles/osx/bin/git-completion.bash"]="~/bin/git-completion.bash"
-    DOTFILE_MAPS["~/.dotfiles/.config/skhd/skhdrc"]="~/.config/skhd/skhdrc"
-    DOTFILE_MAPS["~/.dotfiles/.config/yabai/yabaicmd"]="~/.config/yabai/yabaicmd"
+    DOTFILE_MAPS["osx/.slate"]="~/.slate"
+    DOTFILE_MAPS["osx/.vimperatorrc"]="~/.vimperatorrc"
+    DOTFILE_MAPS["osx/bin/git-completion.bash"]="~/bin/git-completion.bash"
+    DOTFILE_MAPS[".config/skhd/skhdrc"]="~/.config/skhd/skhdrc"
+    DOTFILE_MAPS[".config/yabai/yabaicmd"]="~/.config/yabai/yabaicmd"
   fi
 }
 
@@ -80,7 +82,7 @@ echo_conditions() {
   log info "--- target dotfiles ---"
   for src in "${!DOTFILE_MAPS[@]}"; do
     dst="${DOTFILE_MAPS[$src]}"
-    log info "$src -> $dst"
+    log info "$DOTFILE_DIR$src -> $dst"
   done
   log info "-----------------------"
 }
