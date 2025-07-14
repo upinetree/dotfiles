a1ad74376e97a6e3ef66658153dedc90a29b1d4a
upinetree <upinetree@gmail.com>
2025-07-14 21:19:27 +0900

    連想配列で管理しやすくする

--- scripts/link.sh
+++ scripts/link.sh
@@ -2,6 +2,8 @@ set -u
 
 . ./scripts/lib.sh
 
+declare -A DOTFILE_MAPS
+
 init() {
   detect_platform
   listup_dotfiles
@@ -26,52 +28,49 @@ make_base_dirs() {
 }
 
 unlink_all() {
-  for dotfile_map in "${DOTFILE_MAPS[@]}"; do
-    src_dst=($dotfile_map)
-
-    log "unlink ${src_dst[1]}"
-    eval "unlink ${src_dst[1]}"
+  for src in "${!DOTFILE_MAPS[@]}"; do
+    dst="${DOTFILE_MAPS[$src]}"
+    log "unlink $dst"
+    eval "unlink $dst"
   done
 }
 
 link_all() {
-  for dotfile_map in "${DOTFILE_MAPS[@]}"; do
-    log "ln -s $dotfile_map"
-    eval "ln -s $dotfile_map"
+  for src in "${!DOTFILE_MAPS[@]}"; do
+    dst="${DOTFILE_MAPS[$src]}"
+    log "ln -s $src $dst"
+    eval "ln -s $src $dst"
   done
 }
 
 listup_dotfiles() {
-  export DOTFILE_MAPS
   DOTFILE_MAPS=(
-    "~/.dotfiles/.gemrc                ~/.gemrc"
-    "~/.dotfiles/.gitconfig            ~/.gitconfig"
-    "~/.dotfiles/.gitignore            ~/.gitignore"
-    "~/.dotfiles/.ripgreprc            ~/.ripgreprc"
-    "~/.dotfiles/.tigrc                ~/.tigrc"
-    "~/.dotfiles/.tmux.conf            ~/.tmux.conf"
-    "~/.dotfiles/.vim/colors           ~/.vim/colors"
-    "~/.dotfiles/.vimrc                ~/.vimrc"
-    "~/.dotfiles/.zsh/.aliases.zsh     ~/.zsh/.aliases.zsh"
-    "~/.dotfiles/.zsh/.exports.zsh     ~/.zsh/.exports.zsh"
-    "~/.dotfiles/.zsh/.zplug.zsh       ~/.zsh/.zplug.zsh"
-    "~/.dotfiles/.zshrc                ~/.zshrc"
-    "~/.dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim"
-    "~/.dotfiles/.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml"
-    "~/.dotfiles/.config/mise/.config.toml        ~/.config/mise/config.toml"
-    "~/.dotfiles/default-gems          ~/.config/mise/default-gems"
-    "~/.dotfiles/.claude/settings.json ~/.claude/settings.json"
-    "~/.dotfiles/.claude/commands      ~/.claude/commands"
+    ["~/.dotfiles/.gemrc"]="~/.gemrc"
+    ["~/.dotfiles/.gitconfig"]="~/.gitconfig"
+    ["~/.dotfiles/.gitignore"]="~/.gitignore"
+    ["~/.dotfiles/.ripgreprc"]="~/.ripgreprc"
+    ["~/.dotfiles/.tigrc"]="~/.tigrc"
+    ["~/.dotfiles/.tmux.conf"]="~/.tmux.conf"
+    ["~/.dotfiles/.vim/colors"]="~/.vim/colors"
+    ["~/.dotfiles/.vimrc"]="~/.vimrc"
+    ["~/.dotfiles/.zsh/.aliases.zsh"]="~/.zsh/.aliases.zsh"
+    ["~/.dotfiles/.zsh/.exports.zsh"]="~/.zsh/.exports.zsh"
+    ["~/.dotfiles/.zsh/.zplug.zsh"]="~/.zsh/.zplug.zsh"
+    ["~/.dotfiles/.zshrc"]="~/.zshrc"
+    ["~/.dotfiles/.config/nvim/init.vim"]="~/.config/nvim/init.vim"
+    ["~/.dotfiles/.config/alacritty/alacritty.toml"]="~/.config/alacritty/alacritty.toml"
+    ["~/.dotfiles/.config/mise/.config.toml"]="~/.config/mise/config.toml"
+    ["~/.dotfiles/default-gems"]="~/.config/mise/default-gems"
+    ["~/.dotfiles/.claude/settings.json"]="~/.claude/settings.json"
+    ["~/.dotfiles/.claude/commands"]="~/.claude/commands"
   )
 
   if [ "$PLATFORM" = "osx" ]; then
-    DOTFILE_MAPS+=(
-      "~/.dotfiles/osx/.slate                  ~/.slate"
-      "~/.dotfiles/osx/.vimperatorrc           ~/.vimperatorrc"
-      "~/.dotfiles/osx/bin/git-completion.bash ~/bin/git-completion.bash"
-      "~/.dotfiles/.config/skhd/skhdrc         ~/.config/skhd/skhdrc"
-      "~/.dotfiles/.config/yabai/yabaicmd      ~/.config/yabai/yabaicmd"
-    )
+    DOTFILE_MAPS["~/.dotfiles/osx/.slate"]="~/.slate"
+    DOTFILE_MAPS["~/.dotfiles/osx/.vimperatorrc"]="~/.vimperatorrc"
+    DOTFILE_MAPS["~/.dotfiles/osx/bin/git-completion.bash"]="~/bin/git-completion.bash"
+    DOTFILE_MAPS["~/.dotfiles/.config/skhd/skhdrc"]="~/.config/skhd/skhdrc"
+    DOTFILE_MAPS["~/.dotfiles/.config/yabai/yabaicmd"]="~/.config/yabai/yabaicmd"
   fi
 }
 
@@ -79,8 +78,9 @@ echo_conditions() {
   log info "------- platform ------"
   log info "$PLATFORM"
   log info "--- target dotfiles ---"
-  for map in "${DOTFILE_MAPS[@]}"; do
-    log info "$map"
+  for src in "${!DOTFILE_MAPS[@]}"; do
+    dst="${DOTFILE_MAPS[$src]}"
+    log info "$src -> $dst"
   done
   log info "-----------------------"
 }
